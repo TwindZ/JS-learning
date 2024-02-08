676a737b5aa4f941103336885a5f4f82681715bc
TwindZ <emmanuel.lamothe@gmail.com>
2024-02-08 12:28:58 -0500

    aaaaaaa

--- config.js
+++ config.js
@@ -1,1 +1,3 @@
 const word_list = ["hahaha", "patate", "allo", "hello", "bonjour", "salut"]
+const sentence_list = ["hahaha petit commic", "patate pillé froide", "allo mon amis", "hello my friend",
+ "bonjourles potes", "salut petit canard"]
--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-let answer = prompt("Entré votre réponse")
-console.log(wordTester(answer))
-let test = {
-}
-test.wordTester = wordTester(answer)
-test.bonjour()
-test.wordTester()
-function wordTester(answer){
-if(answer === "salut")
-	return "oui cest ca"
-else
-	return "non cest pas ca"
-}
+// let balise_to_write = document.getElementById("to_write")
+// console.log(balise_to_write.clientHeight)
+// let balise_to_write2 = document.querySelector("#to_write span")
+// console.log(balise_to_write2)
+// let balise_radio = document.querySelectorAll(".zoneChoix input")
+// console.log(balise_radio)
+// for (let i = 0; i < balise_radio.length; i++)
+// 	console.log(balise_radio[i])
+
+// let answer = prompt("Entré votre réponse")
+// console.log(wordTester(answer))
+// let test = {
+// }
+// test.wordTester = wordTester(answer)
+// test.bonjour()
+// test.wordTester()
+// function wordTester(answer){
+// if(answer === "salut")
+// 	return "oui cest ca"
+// else
+// 	return "non cest pas ca"
+// }
+
+let answer = document.getElementById("answer")
+let bt_submit = document.getElementById("btSubmit")
+let to_write = document.querySelector("#toWrite")
+let score_count = document.querySelector("#scoreCount")
+let mode = document.querySelectorAll(".mode input")
+console.log(mode)
