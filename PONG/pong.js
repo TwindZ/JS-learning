e7f899dfd903cde2a36fc4b9f648b44deadbee0b
TwindZ <emmanuel.lamothe@gmail.com>
2024-02-13 23:06:08 -0500

    pong base mechanic ok

--- PONG/pong.js
+++ PONG/pong.js
@@ -1,59 +1,113 @@
-let paddleSpeed = 20
-let ballBaseSpeed = 5
-let ballSpeed = ballBaseSpeed
+let paddleSpeed = 5
+let ballBaseSpeed = 7
+let ballSpeedX = ballBaseSpeed
+let ballSpeedY = 1
 let paddleLeftTag = document.getElementById("paddleLeft")
 let paddleRightTag = document.getElementById("paddleRight")
 let ballTag = document.getElementById("ball")
+let gameoverTag = document.getElementById("gameover")
 
 let ball = {
-	posX : 200,
-	posY : 400,
+	posX : 500,
+	posY : 500,
+	diameter: 50,
 	dirX : 1,
-	dirY : -1
+	dirY : 1,
+	hitBoxLeft: 0,
+	hitBoxRight: 0,
+	hitBoxTop: 0,
+	hitBoxBottom: 0,
 }
 
 let paddleLeft = {
 	pos : 350,
+	height: 300,
 	dir1 : false,
-	dir2 : false
+	dir2 : false,
+	hitBoxStart : 0,
+	hitBoxEnd : 0,
+	hitBoxX: 50 //a define width + posx
 }
 let paddleRight = {
 	pos : 350,
 	dir1 : false,
-	dir2 : false
+	dir2 : false,
+	hitBoxStart : 0,
+	hitBoxEnd : 0,
 }
+
+function hitBoxesUpdate(){
+	ball.hitBoxLeft = ball.posX
+	console.log("left :" + ball.hitBoxLeft)
+	ball.hitBoxTop = ball.posY
+	console.log("top :" + ball.hitBoxTop)
+	ball.hitBoxBottom = ball.posX + (ball.diameter)
+	console.log("Bottom :" + ball.hitBoxBottom)
+	ball.hitBoxRight = ball.posY + (ball.diameter)
+	console.log("Right :" + ball.hitBoxRight)
+
+	paddleLeft.hitBoxStart = paddleLeft.pos
+	console.log("Left start :" + paddleLeft.hitBoxStart)
+	paddleLeft.hitBoxEnd = paddleLeft.pos + paddleLeft.height
+	console.log("left end :" + paddleLeft.hitBoxEnd)
+
+	paddleRight.hitBoxStart = paddleRight.pos
+	console.log("Right start :" + paddleRight.hitBoxStart)
+	paddleRight.hitBoxEnd = paddleRight.pos + paddleLeft.height// avoir const paddle size
+	console.log("right end :" + paddleRight.hitBoxEnd)
+
+}
+
 
 function randAngle(){
 	return Math.random();
 }
 
 function moveBall(){
+	hitBoxesUpdate()
 	if(ball.dirX > 0){
-		ball.posX += (ballSpeed)
+		ball.posX += ballSpeedX
 	}
 	if(ball.dirX < 0){
-		ball.posX -= ballSpeed
+		ball.posX -= ballSpeedX
 	}
 	if(ball.dirY > 0){
-		ball.posY += ballSpeed
+		ball.posY += ballSpeedY
 	}
 	if(ball.dirY < 0){
-		ball.posY -= ballSpeed
+		ball.posY -= ballSpeedY
 	}
-	if(ball.posX === 960)
+	if(ball.hitBoxLeft >= 900/* width + posx de paddle right*/ && ball.posY + (ball.diameter / 2) >= paddleRight.hitBoxStart && ball.posY <= paddleRight.hitBoxEnd){
 		ball.dirX = -1
-	if(ball.posX === 0)
-		ball.dirX = 1
-	if(ball.posY === 960)
+		ballSpeedY = Math.floor((Math.random() * 10) + 1)
+	}
+	if(ball.posX > 960){
+		ball.dirX = 0
+		ball.dirY = 0
+		gameoverTag.style.display = "block"
+
+	}
+
+	if(ball.hitBoxLeft <= 50/* width + posx de paddle left*/ && ball.posY + (ball.diameter / 2) >= paddleLeft.hitBoxStart && ball.posY <= paddleLeft.hitBoxEnd){
+	ball.dirX = 1
+	ballSpeedY = Math.floor((Math.random() * 10) + 1)
+
+	}
+	if(ball.posX <  5){
+		ball.dirX = 0
+		ball.dirY = 0
+		gameoverTag.style.display = "block"
+	}
+	if(ball.posY > 960)
 		ball.dirY = -1
-	if(ball.posY === 0)
+	if(ball.posY < 5)
 		ball.dirY = 1
 
 	ballTag.style.top = ball.posY + "px"
 	ballTag.style.left = ball.posX + "px"
 	paddleLeftTag.style.top = paddleLeft.pos + "px"
 	requestAnimationFrame(moveBall);
-	console.log()
+	// console.log()
 }
 
 function movePaddles() {
