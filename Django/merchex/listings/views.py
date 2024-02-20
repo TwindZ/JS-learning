37b47c594d57a50a11e1498cffc2dad3cef4cfc9
TwindZ <emmanuel.lamothe@gmail.com>
2024-02-20 12:57:28 -0500

    ataboy

--- Django/merchex/listings/views.py
+++ Django/merchex/listings/views.py
@@ -1,11 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from listings.models import Band
+from listings.models import Affiche
 
 def hello(request):
-	return HttpResponse('<h1>Hello World!</h1>')
+	bands = Band.objects.all()
+	return render(request, 'listings/hello.html', {'bands': bands})
 
 def about(request):
-	return HttpResponse('<h1>À propos</h1> <p>BlaBla de caca poil</p>')
+	affiche = Affiche.objects.all()
+	return HttpResponse(f"""
+	 	<h1>Affiche!</h1>
+		<p>liste des affiches :<p>
+		<ul>
+			<li>{affiche[0].title}</li>
+			<li>{affiche[1].title}</li>
+			<li>{affiche[2].title}</li>
+			<li>{affiche[3].title}</li>
+		</ul>
+""")
 
 def listings(request):
 	return HttpResponse('<h1>Listings</h1> <p>Liste d\'article</p>')
